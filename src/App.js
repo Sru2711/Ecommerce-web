1c7d7e91e8f5c2ffb7c3b81cf890aeef477e0b18
Sru2711 <srushtimalagi@gmail.com>
2024-09-11 12:10:33 +0530

    Navbar created

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.scss';
+import { useRoutes } from 'react-router-dom';
+import { Button } from 'reactstrap';
+import drawer from "../src/Assets/drawer.png"
+import { useState } from 'react';
+import Navbar from './components/Navbar';
+function App() {
+  const [toggle, setToggle] = useState(false)
+
+  return (
+    <div className="App">
+
+      <div className={toggle ? "navbar-divopen" : "navbar-divclose"}>
+        <div className={`toggler ${toggle ? "drawer-divopen" : "drawer-divclose"}`}>
+          <img src={drawer} className="drawer-img" onClick={() => { setToggle(!toggle) }}></img>
+        </div>
+        <Navbar></Navbar>
+      </div>
+      <div className={toggle ? "content-divopen" : "content-divclose"}>
+        hello
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Row } from 'reactstrap'
+import "../style/Navbar.scss"
+const Navbar = ({toggle}) => {
+  return (
+    <div>
+      
+    </div>
+  
+    
+  )
+}
+
+export default Navbar
